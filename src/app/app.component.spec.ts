5843781e4388318b958143cf7b60bd21f9355409
iromashko <iromashko@me.com>
2020-11-18 23:40:26 +0300

    refactor

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng-storybook'`, () => {
+  it(`should have as title 'AngularStorybook'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-storybook');
+    expect(app.title).toEqual('AngularStorybook');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ng-storybook app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('AngularStorybook app is running!');
   });
 });
